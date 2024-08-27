34c7d4adadefb84f017272e2d14211efcf9f9899
Yehor Motornyi <egormotot@gmail.com>
2024-08-27 23:54:49 +0300

    Messagge!

--- src/main/java/com/example/springbootpages/Controller/Hello.java
+++ src/main/java/com/example/springbootpages/Controller/Hello.java
@@ -2,6 +2,6 @@ package com.example.springbootpages.Controller;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!!!");
     }
 }
